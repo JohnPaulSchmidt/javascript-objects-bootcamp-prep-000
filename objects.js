667d33889cb46551414ab6511c2b50c6bdd7fd62
John Paul Schmidt <mc.wren@gmail.com>
2018-08-23 21:15:23 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,5 +2,5 @@ var playlist = {'Phil Ochs': 'Here\'s to the State of Mississippi'};
 
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist['Here\'s to the State of Mississippi'] = songTitle
-  return updatePlaylist()
+  return playlist
 }
