a7a9047a59a914080f635b489d8ee0dfde77e51e
John Paul Schmidt <mc.wren@gmail.com>
2018-08-23 21:09:52 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,5 +3,5 @@ var playlist = {'Jeff Buckley': 'Lover, You Should Have Come Over'
 
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist['Lover, You Should Have Come Over'] = songTitle
-  console.log(updatePlaylist)
+  return updatePlaylist()
 }
