f55c9e15960598be61aea49d951722a1d8b6a5d2
John Paul Schmidt <mc.wren@gmail.com>
2018-08-23 21:27:35 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,3 +4,7 @@ function updatePlaylist(playlist, artistName, songTitle) {
   playlist['Here\'s to the State of Mississippi'] = songTitle
   return playlist
 }
+
+function removeFromPlaylist(playlist, artistName) {
+  delete artistName.'Phil Ochs';
+}
